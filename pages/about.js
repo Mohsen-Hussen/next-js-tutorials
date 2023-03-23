645e990ef86b0b87a172170de9c8d569f3f57c6b
Mohsen Omar <moomar.c@tawal.com.sa>
2023-03-23 12:20:54 +0300

    add styling section

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/About.module.css";
+
+const About = () => {
+	return (
+		<div>
+			<h2 className={styles.higlight}>About Page</h2>
+			<button className="btn btn-primary">Primary</button>
+		</div>
+	);
+};
+
+export default About;
